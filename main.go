1d9721499f711726fb347c66b617e4c51968c320
priom <priom.galaxy@gmail.com>
2018-06-03 00:38:40 -0400

    simple rest api in go

---
+++ main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"strconv"
+	"encoding/json"
+	"log"
+	"net/http"
+	"math/rand"
+	// "strconv"
+	"github.com/gorilla/mux"
+)
+
+// book struct
+type Book struct {
+	ID string `json:"id"`
+	ISBN string `json:"isbn"`
+	Title string `json:"title"`
+	Author *Author `json:"author"`
+}
+
+// author struct
+type Author struct {
+	Firstname string `json:"firstname"`
+	Lastname string `json:"lastname"`
+}
+
+// init books var as a slice Book struct
+var books []Book
+
+// get all books
+func getBooks(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(books)
+}
+
+// get single book
+func getBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
+}
+
+// get all books
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var book Book
+	_ = json.NewDecoder(r.Body).Decode(&book)
+	book.ID = strconv.Itoa(rand.Intn(10000000))
+	books = append(books, book)
+	json.NewEncoder(w).Encode(book)
+}
+
+// get all books
+func updateBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			var book Book
+			_ = json.NewDecoder(r.Body).Decode(&book)
+			book.ID = params["id"]
+			books = append(books, book)
+			json.NewEncoder(w).Encode(book)
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+// get all books
+func deleteBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range books {
+		if item.ID == params["id"] {
+			books = append(books[:index], books[index+1:]...)
+			break
+		}
+	}
+	json.NewEncoder(w).Encode(books)
+}
+
+func main() {
+	// init router
+	r := mux.NewRouter()
+
+	// mock data
+	books = append(books, Book{ID: "1", ISBN: "420420", Title: "Book One", Author: &Author {Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", ISBN: "069069", Title: "Book Two", Author: &Author {Firstname: "Jane", Lastname: "Doe"}})
+
+	// api endpoints
+	r.HandleFunc("/api/books", getBooks).Methods("GET")
+	r.HandleFunc("/api/books/{id}", getBook).Methods("GET")
+	r.HandleFunc("/api/books", createBook).Methods("POST")
+	r.HandleFunc("/api/books/{id}", updateBook).Methods("PUT")
+	r.HandleFunc("/api/books/{id}", deleteBook).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
